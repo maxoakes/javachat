80206ca1dafda2b6d94edb91dc865319a8816614
Max <m.oakes017@gmail.com>
2016-10-26 17:58:52 -0700

    started the project, began join routine for server, not working now

---
+++ Client.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * A client that connects to a server on startup
+ * Client must input a username when prompted
+ * The client then connect to the server
+ * The user can type messages and send them to the server,
+ * and the user can wait and see messages from other users displayed in the terminal
+ * 
+ * @author Justin Ohta, Max Oakes
+ * @version 1.0.0
+ */
+public class Client {
+    
+    public static void main(String[] args) throws IOException {
+        // create Strings to store message and reply
+        String message;
+		String reply;
+		String username;
+        String address;
+		
+		//get the username
+		System.out.print("\nUsername: ");
+		username = System.console().readLine();
+		
+		//get address
+		System.out.print("\nServer IP: ");
+		address = System.console().readLine();
+		
+        try {
+            // create reader to acquire text
+            BufferedReader userIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        
+            // get message from user
+            message = userIn.readLine();
+        
+            //connect via address given, send username with it
+            Socket clientSocket = new Socket(address, 2016);
+			
+			//Read before you can write so no messages are lost when coming to you
+			BufferedReader serverIn = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+			DataOutputStream serverOut = new DataOutputStream(clientSocket.getOutputStream());
+			serverOut.writeBytes(username);
+            
+            // create stream for reading data from server
+
+            
+            // send data to server
+            serverOut.writeBytes(message + '\n');
+            // get data from server
+            reply = serverIn.readLine();
+            // print reply from server
+            System.out.println("REPLY RECEIVED: " + reply);
+            clientSocket.close();
+        }
+        catch(ConnectException e) {
+            System.out.println("Error connecting to server. Check that server is running and accepting connections.");
+        }
+        catch(Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.*;
+import java.time.*;
+
+/**
+ * A server that manages a multi-user chat program
+ * This server takes strings that are read from internet users
+ * attaches a timestamp and username with it, and sends it to all clients connected
+ * Will also print the full message to a file on server's computer
+ * 
+ * @author Max Oakes, Justin Ohta
+ * @version 1.0.0
+ */
+public class Server {
+	
+    //Start a server
+    public static void main(String[] args) throws IOException
+	{
+        String clientMessage;
+		String serverReply;
+		String[] username; //hard cap, if connectionNumber goes above it, it'll break
+        Socket[] connection;
+		int connectionNumber = 0;
+		
+        try
+		{
+            //made a port
+            ServerSocket accepting = new ServerSocket(2016); //port 2016, because why not
+            
+            // wait for clients to make connections
+            while(true)
+			{
+				//when user connects
+                connection[connectionNumber] = accepting.accept();
+				BufferedReader clientIn = new BufferedReader(new InputStreamReader(connection[connectionNumber].getInputStream()));
+				username[connectionNumber] = clientIn.readLine();
+				System.out.println("User Connected: " + username[connectionNumber] + " from " + connection[connectionNumber].getInetAddress());
+				//Ready to send that new user messages
+                DataOutputStream clientOut = new DataOutputStream(connection[connectionNumber].getOutputStream());
+				String welcome = "Welcome to the server, " + username[connectionNumber] +"!\n";
+				connectionNumber++;
+				
+                // get message from client and captilatize the letters
+                //clientMessage = clientIn.readLine();
+                //System.out.println("Got the message: " + clientMessage);
+                //serverReply = "Ack for client at " + connectionSocket.getInetAddress() + ": " + clientMessage.toUpperCase() + "\n";
+
+		            // send client reply 
+                //clientOut.writeBytes(serverReply);
+                //connectionSocket.close();
+            }
+        }
+        catch (Exception e)
+		{
+            System.out.println("An error occurred while creating server socket or reading/writing data to/from client.");
+        }
+    }
+}
