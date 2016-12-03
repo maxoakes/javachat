1e04a2b7ec9930b2923530c634921cd7ce4a42e2
Max <m.oakes017@gmail.com>
2016-12-02 17:40:30 -0800

    added multi-user support, added timestamps, added local log file

--- Server.java
+++ Server.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.net.*;
 import java.time.*;
 import java.lang.*;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * A server that manages a multi-user chat program
@@ -15,8 +18,13 @@ import java.lang.*;
 public class Server implements Runnable{
 	
 	private static final int MAX_USER = 50;
-	private static BufferedReader clientIn;
-	private static DataOutputStream clientOut;
+	private static BufferedReader[] clientIn = new BufferedReader[MAX_USER];
+	private static DataOutputStream[] clientOut = new DataOutputStream[MAX_USER];
+	private static String[] username = new String[MAX_USER];
+	private static Socket[] connection = new Socket[MAX_USER];
+	private static Thread[] messageHandler = new Thread[MAX_USER];
+	private static int connectionNumber = 0;
+	private static PrintWriter log;
 	
     //Start a server
     public static void main(String[] args) throws IOException
@@ -24,19 +32,17 @@ public class Server implements Runnable{
         String clientMessage;
 		String serverReply;
 		
-		String[] username = new String[MAX_USER]; //hard cap, if connectionNumber goes above it, it'll break
-		
-        Socket[] connection = new Socket[MAX_USER];
-		int connectionNumber = 0;
-		
         try
 		{
+			//open file for logging
+			log = new PrintWriter("server.log", "UTF-8");
+			
             //made a port
             ServerSocket accepting = new ServerSocket(2016); //port 2016, because why not
             
             // wait for clients to make connections
 			//this new thread will manage incoming chat
-			(new Thread(new Server())).start();
+			//(new Thread(new Server())).start();
 			
 			//current thread will manage incoming users
 			System.out.println("SERVER - Thread Spawned, in main thread now\n");
@@ -47,21 +53,23 @@ public class Server implements Runnable{
 				System.out.println("Awaiting users to join\n");
 				//accept their connection
                 connection[connectionNumber] = accepting.accept();
+				messageHandler[connectionNumber] = new Thread(new Server());
+				messageHandler[connectionNumber].start();
 				
 				//get a stream from client to this server
-				clientIn = new BufferedReader(new InputStreamReader(connection[connectionNumber].getInputStream()));
-				username[connectionNumber] = clientIn.readLine();
+				clientIn[connectionNumber] = new BufferedReader(new InputStreamReader(connection[connectionNumber].getInputStream()));
+				username[connectionNumber] = clientIn[connectionNumber].readLine();
 				System.out.println("User Connected: " + username[connectionNumber] + " from " + connection[connectionNumber].getInetAddress() + "\n");
 				
 				//get a stream from server to client
-                clientOut = new DataOutputStream(connection[connectionNumber].getOutputStream());
+                clientOut[connectionNumber] = new DataOutputStream(connection[connectionNumber].getOutputStream());
 				
 				//send welcome message
 				String welcome = "Welcome to the server, " + username[connectionNumber] + "!\n";
-				clientOut.writeBytes(welcome);
+				clientOut[connectionNumber].writeBytes(welcome);
 				
 				//iterate connection for new user
-				//connectionNumber++;
+				connectionNumber++;
             }
         }
         catch (Exception e)
@@ -74,6 +82,28 @@ public class Server implements Runnable{
 	//Its job is to manage incoming messages
 	public void run()
 	{
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			@Override
+			public void run()
+			{
+				log.close();
+				System.out.println("Server closed");
+				for (int i = 0; i<connectionNumber; i++)
+				{
+					try
+					{
+						clientOut[i].writeBytes("Server closed\n");
+					}
+					catch (Exception e)
+					{
+						System.out.println("Error when sending\n");
+					}
+				}
+			}
+		});
+		
+		int thisThreadNum = connectionNumber;
+		System.out.println("In new thread #"+thisThreadNum);
 		//init next chat message
 		String msg = "";
 		
@@ -83,8 +113,9 @@ public class Server implements Runnable{
 			try
 			{
 				//wait for new chat message
-				msg = clientIn.readLine();
+				msg = clientIn[thisThreadNum].readLine();
 				
+				//full message and message type has first chars, /chat for example
 				//System.out.println(msg+"\n");
 				
 				//dont tell me when nobody is sending anything
@@ -123,18 +154,34 @@ public class Server implements Runnable{
 					//get the string of the chat
 					msg = msg.substring(5);
 					
+					//Do string operations: add timestamp
+					long time = System.currentTimeMillis();
+					Date date = new Date(time);
+					DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+					//System.out.println(formatter.format(date));
+					
+					String fullMessage = "[" + formatter.format(date) + "] " + msg + '\n';
+					
 					//print it so the server (and later a log)
-					System.out.println(msg);
-					try
+					System.out.println(fullMessage);
+					for (int i = 0; i<connectionNumber; i++)
 					{
-						//send it to all clients (currently just one client)
-						clientOut.writeBytes(msg+'\n');
-						//System.out.println("DEBUG - MSG SENT: "+ msg + "\n");
+						try
+						{
+							//send it to all clients (currently just one client)
+							clientOut[thisThreadNum].writeBytes(fullMessage);
+							
+							//print to log file
+							
+							log.println(fullMessage);
+							//System.out.println("DEBUG - MSG SENT: "+ msg + "\n");
+						}
+						catch (Exception e)
+						{
+							System.out.println("Error when sending\n");
+						}
 					}
-					catch (Exception e)
-					{
-						System.out.println("Error when sending\n");
-					}
+
 				}
 			}
 			//reset the next message
